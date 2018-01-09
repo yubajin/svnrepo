899ddc4a69cdd7464d7e7809fb1af63e944d81a4
yubajin <1803279085@qq.com>
2018-01-09 17:46:09 +0800

    init commit

---
+++ src/com/tencent/wechatjump/helper/Main.java
@@ -0,0 +1,107 @@
+package com.tencent.wechatjump.helper;
+
+import com.tencent.wechatjump.helper.util.HelperUtil;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+
+/**
+ * Created by xushanmeng on 2017/12/30.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+    	HelperUtil.execute("adb devices");
+        System.out.println("目前已适配手机分辨率\n" +
+                "  a) 1600x2560，跳跃系数0.92\n" +
+                "  b) 1440x2560，跳跃系数1.039\n" +
+                "  c) 1080x1920，跳跃系数1.392\n" +
+                "  d) 720x1280，跳跃系数2.078\n");
+        System.out.println("正在检测手机分辨率，请稍候...");
+        File imageFile = new File("screen.png");
+        if (imageFile.exists()) {
+            imageFile.delete();
+        }
+        boolean result = HelperUtil.execute("adb shell screencap -p /sdcard/screen.png");
+        if (!result) {
+            System.out.println("手机分辨率检测失败，请检查电脑与手机连接和手机设置。");
+            return;
+        }
+        result = HelperUtil.execute("adb pull /sdcard/screen.png .");
+        if (!result) {
+            System.out.println("手机分辨率检测失败，请检查电脑与手机连接和手机设置。");
+            return;
+        }
+        int screenWidth;
+        int screenHeight;
+        try {
+            BufferedImage image = ImageIO.read(imageFile);
+            screenWidth = image.getWidth();
+            screenHeight = image.getHeight();
+        } catch (Exception e) {
+            System.out.println("手机分辨率检测失败，请检查电脑与手机连接和手机设置。");
+            return;
+        }
+        if (screenWidth == 0 || screenHeight == 0) {
+            System.out.println("手机分辨率检测失败，请检查电脑与手机连接和手机设置。");
+            return;
+        }
+        System.out.println("检测到您的手机分辨率为" + screenWidth + "x" + screenHeight);
+        String screenStr = screenWidth + "x" + screenHeight;
+        double jumpParam = 0;
+        switch (screenStr) {
+            case "1600x2560":
+                jumpParam = Constants.JUMP_PARAM_1600x2560;
+                break;
+            case "1440x2560":
+                jumpParam = Constants.JUMP_PARAM_1440x2560;
+                break;
+            case "1080x1920":
+                jumpParam = Constants.JUMP_PARAM_1080x1920;
+                break;
+            case "720x1280":
+                jumpParam = Constants.JUMP_PARAM_720x1280;
+                break;
+        }
+        if (jumpParam == 0) {
+            System.out.print("暂时不兼容您的手机分辨率，请直接输入您的跳跃系数：");
+        }
+        System.out.print("推荐跳跃系数为"+jumpParam+"，是否使用系统推荐跳跃系数？(y/n)");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String line = scanner.nextLine();
+            if("y".equalsIgnoreCase(line.trim()) || "yes".equalsIgnoreCase(line.trim())){
+                break;
+            }else if("n".equalsIgnoreCase(line.trim()) || "no".equalsIgnoreCase(line.trim())){
+                jumpParam = 0;
+                System.out.print("请输入您的跳跃系数：");
+                break;
+            }else{
+                System.out.print("命令输入错误，请重新输入：");
+            }
+        }
+        while (jumpParam == 0) {
+            String line = scanner.nextLine();
+            if (line == null) {
+                continue;
+            }
+            try {
+                jumpParam = Double.valueOf(line);
+            } catch (Exception e) {
+            }
+            if (jumpParam == 0) {
+                System.out.print("输入格式错误，请重新输入：");
+            }
+        }
+        System.out.println("正在开始自动跳一跳...");
+        new Helper(jumpParam).start();
+    }
+}
